3104e80afb5f38215eecadad58246c7df5438d51
Afrokk <afrasiyabkhan379@gmail.com>
2022-11-21 15:08:58 -0500

    Updated Solution

--- solution.py
+++ solution.py
@@ -1,7 +1,9 @@
+#o(n) solution
+
 def calcProfit(stockPrices):
     # Initializing two pointers to go through the array in a single loop instead of 
     # the double for-loops approach that I took in the brute force solution.'
-    
+
     # "first" checks the stock price, "second" checks the second price (selling price)
     first, second = 0, 1
     maxProfit = 0
@@ -10,18 +12,18 @@ def calcProfit(stockPrices):
     for i in range(second, numDays):
         # Calculating current profit between two days.
         profit = stockPrices[second] - stockPrices[first]
-        
+
         # If current profit is more than the maxProfit, we set new maxProfit.
         if profit > maxProfit:
             maxProfit = profit
-        
+
         # If the profit is less than the maxProfit, we move forward to the next
         # two days to check if we can find a better profit.
-        
+
         elif stockPrices[second] < stockPrices[first]:
             first = second
         second += 1
-    
+
     return maxProfit
 
 # Tester
