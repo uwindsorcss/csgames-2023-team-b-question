7915c1e2295cf7c7421d4a101f13c8f0d2b59e82
Nafiz1 <nafizh007@gmail.com>
2022-11-21 02:13:38 -0500

    made function

--- solution.java
+++ solution.java
@@ -4,17 +4,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class solution {
-    public static void main(String[] args) throws IOException {
-        ArrayList<Integer> stocks = new ArrayList<Integer>();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        String[] rawInput;
-        rawInput = br.readLine().split(" ");
-
-        for (int i = 0; i < rawInput.length; i++) {
-            stocks.add(Integer.parseInt(rawInput[i]));
-        }
-
+    public static int maxProfit(ArrayList<Integer> stocks) {
         int lowest = Integer.MAX_VALUE;
         int maxProfit = 0;
         int profit = 0;
@@ -29,6 +19,20 @@ public class solution {
             }
         }
 
-        System.out.println(maxProfit);
+        return maxProfit;
+    }
+
+    public static void main(String[] args) throws IOException {
+        ArrayList<Integer> stocks = new ArrayList<Integer>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] rawInput;
+        rawInput = br.readLine().split(" ");
+
+        for (int i = 0; i < rawInput.length; i++) {
+            stocks.add(Integer.parseInt(rawInput[i]));
+        }
+
+        System.out.println(maxProfit(stocks));
     }
 }
