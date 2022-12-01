20eee14bf4bdc3946c7df56edeaa23396eb129c9
DanielThePear <blockstreaker61@gmail.com>
2022-12-01 13:26:19 -0500

    Added comments and type hints

--- solution.py
+++ solution.py
@@ -1,15 +1,15 @@
-def main():
+def main() -> None:
 
-    stocks = [int(each) for each in input("Please enter the input: ").split()]
-    print(max_profit(stocks))
+    stocks: list[int] = [int(each) for each in input("Please enter the input: ").split()] # receive space-separated list of prices, as defined in the README, and parse to list of ints
+    print(max_profit(stocks)) # generates fairy dust or something idk, the print() function will forever be a mystery to me
 
-def max_profit(stocks):
+def max_profit(stocks: list[int]) -> int:
 
-    min_price = stocks[0]
-    max_profit = stocks[1] - min_price
+    min_price: int = stocks[0] # the lowest price to buy at
+    max_profit: int = stocks[1] - min_price # the profit made by the best buy/sell combo found so far
 
     for price in stocks[1:]:
-        if price < min_price: min_price = price
+        if price < min_price: min_price = price # we have found a better value to buy at
         elif price - min_price > max_profit: max_profit = price - min_price
 
     if max_profit <= 0: return 0 # there isn't a single profitable trade in the array
